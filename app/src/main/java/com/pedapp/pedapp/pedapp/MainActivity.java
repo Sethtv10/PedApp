38cddda2d5f41d8fbe2f2d920fdd3a283fab9c21
sethtv10 <al02812766@tecmilenio.mx>
2018-11-02 02:08:01 -0600

    first commit

---
+++ app/src/main/java/com/pedapp/pedapp/pedapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.pedapp.pedapp.pedapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+    public int players = 2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void plusButton(View view) {
+            LinearLayout ll = (LinearLayout) findViewById(R.id.playerslayout);
+            EditText et = new EditText(this);
+            et.setId(players);
+            et.setHint("Nombre de Jugador");
+            et.setPadding(10,40,10,30);
+            ll.addView(et);
+        players = players + 1;
+        EditText numberPlayers = (EditText) findViewById(R.id.numberOfPlayers);
+        numberPlayers.setText(String.valueOf(players));
+
+    }
+    public void lessButton(View view) {
+        if (players > 2) {
+            players = players - 1;
+            LinearLayout ll = (LinearLayout) findViewById(R.id.playerslayout);
+            EditText editText = (EditText) findViewById(players);
+            ll.removeView(editText);
+            EditText numberPlayers = (EditText) findViewById(R.id.numberOfPlayers);
+            numberPlayers.setText(String.valueOf(players));
+        }
+
+    }
+
+
+}
